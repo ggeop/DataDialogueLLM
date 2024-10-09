fc00e1217229dd50f58ff21cb272d7fbada62ea3
ggeop <giorgos.papachristou@metis.tech>
2024-10-09 17:37:07 +0300

    Fix generate response object

--- backend/app/api/endpoints/generate.py
+++ backend/app/api/endpoints/generate.py
@@ -6,14 +6,14 @@ from fastapi import (
     status
 )
 
-from app.schemas import Query, GeneralResponse
+from app.schemas import Query, DialogueResult
 from app.services.data_dialogue import data_dialogue_service
 
 router = APIRouter()
 logger = logging.getLogger(__name__)
 
 
-@router.post("/generate", response_model=GeneralResponse)
+@router.post("/generate", response_model=DialogueResult)
 async def generate_text(query: Query):
     try:
         agent = data_dialogue_service.get_agent(query.model)
