b8baa4b180dc7f4c110d2f2f2591afbcda7b78a2
ggeop <giorgos.papachristou@metis.tech>
2024-11-24 12:57:55 +0200

    feat(backend): Improve apis response objects

--- backend/app/schemas/__init__.py
+++ backend/app/schemas/__init__.py
@@ -2,14 +2,28 @@ from app.schemas.database import (
     DatabaseCreationResponse,
 )
 from app.schemas.agents import RegisterAgent
-from app.schemas.response import SQLResponse, GeneralResponse, DialogueResult
+from app.schemas.responses import (
+    BaseResponse,
+    DataResponse,
+    PaginatedResponse,
+    ErrorResponse,
+    AgentList,
+    SQLResponse,
+    GeneralResponse,
+    DialogueResult
+)
 from app.schemas.query import Query
 
 
 __all__ = [
+    BaseResponse,
+    DataResponse,
+    PaginatedResponse,
+    ErrorResponse,
     Query,
     DatabaseCreationResponse,
     RegisterAgent,
+    AgentList,
     SQLResponse,
     GeneralResponse,
     DialogueResult,
